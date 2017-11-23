ac1b3ae2f927cda9910536911a96a0e6fc41a569
Andrew Meads <andrew.meads@auckland.ac.nz>
2017-11-24 11:07:51 +1300

    Initial commit

---
+++ src/ictgradschool/industry/lab07/ex05/ExerciseFive.java
@@ -0,0 +1,20 @@
+package ictgradschool.industry.lab07.ex05;
+
+/**
+ * TODO Write a program according to the Exercise Five guidelines in your lab handout.
+ */
+public class ExerciseFive {
+
+    public void start() {
+        // TODO Write the codes :)
+    }
+
+    // TODO Write some methods to help you.
+
+    /**
+     * Program entry point. Do not edit.
+     */
+    public static void main(String[] args) {
+        new ExerciseFive().start();
+    }
+}
